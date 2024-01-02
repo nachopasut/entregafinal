8ba42cf7144878c6df8db8ad71a8774af29426da
nachopasut <138828086+nachopasut@users.noreply.github.com>
2024-01-02 14:00:10 +0000

    arreglando

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import CartProvider from './components/context/CartContext'
--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -1,5 +1,6 @@
 import Item from "../Item";
 import React from "react";
+
 const ItemList = ({data = []}) => {
     return (
        data.map(film => <Item key={film.id} info={film} />)
